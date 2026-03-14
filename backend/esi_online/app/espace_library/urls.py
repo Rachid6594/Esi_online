c64311b1416baa156940e743e3e53d60fa03b4dc
NinjaCopieur <josephilboudo6@gmail.com>
2026-03-14 21:00:59 +0000

    Étape 3.4 — URLs Bibliothécaire

--- backend/esi_online/app/espace_library/urls.py
+++ backend/esi_online/app/espace_library/urls.py
@@ -3,10 +3,18 @@ URLs pour l'application espace_bibliotheque.
 """
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from app.espace_library.views.espace_bibliotheque_viewset import EspaceBibliothequeViewSet
+from app.espace_library.views import *
 
 router = DefaultRouter(trailing_slash=False)
 router.register(r"", EspaceBibliothequeViewSet, basename="espace_bibliotheque")
+router.register(r"auteurs",       AuteurViewSet,             basename="auteur")
+router.register(r"categories",    CategorieViewSet,          basename="categorie")
+router.register(r"collections",   CollectionViewSet,         basename="collection")
+router.register(r"livres",        LivreViewSet,              basename="livre")
+router.register(r"rapports",      RapportSoutenanceViewSet,  basename="rapport")
+router.register(r"devoirs",       DocumentDevoirViewSet,     basename="devoir")
+router.register(r"signalements",  SignalementViewSet,        basename="signalement")
+
 
 urlpatterns = [
     path("", include(router.urls)),
